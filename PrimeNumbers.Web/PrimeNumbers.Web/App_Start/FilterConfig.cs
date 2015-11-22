8d0992ff47c4f0a85042367afb5884c8869e3dcc
Joaquin Corchero <joaquin.corchero@gmail.com>
2015-11-22 00:04:57 +0000

    Creation of the web app with tests

---
+++ PrimeNumbers.Web/PrimeNumbers.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeNumbers.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PrimeNumbers.Web/PrimeNumbers.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrimeNumbers.Web.MvcApplication" Language="C#" %>
