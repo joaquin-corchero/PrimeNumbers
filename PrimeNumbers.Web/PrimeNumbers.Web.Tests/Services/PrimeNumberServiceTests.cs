19d2bbea88e696e15ef4214bc03b70423b159e23
Joaquin Corchero <joaquin.corchero@gmail.com>
2015-11-22 00:32:34 +0000

    Creation of first test

--- PrimeNumbers.Web/PrimeNumbers.Web.Tests/Services/PrimeNumberServiceTests.cs
+++ PrimeNumbers.Web/PrimeNumbers.Web.Tests/Services/PrimeNumberServiceTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NBehave.Spec.MSTest;
 using PrimeNumbers.Web.Services;
+using System.Linq;
 
 namespace PrimeNumbers.Web.Tests.Services
 {
@@ -16,12 +17,14 @@ namespace PrimeNumbers.Web.Tests.Services
     [TestClass]
     public class and_getting_the_n_first_primes : when_working_with_the_prime_number_service
     {
-
-
         [TestMethod]
         public void then_n_number_of_items_are_returned()
         {
+            int expected = 5;
 
+            List<int> actual = _service.GetFirstPrimes(expected);
+
+            actual.Count().ShouldEqual(expected);
         }
     }
 }
