48414e482e5f72e64662edee576b2beee092ccdb
Joaquin Corchero <joaquin.corchero@gmail.com>
2015-11-22 00:30:02 +0000

    Addition of the test class for the prime number service and the service itself

---
+++ PrimeNumbers.Web/PrimeNumbers.Web.Tests/Services/PrimeNumberServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NBehave.Spec.MSTest;
+using PrimeNumbers.Web.Services;
+
+namespace PrimeNumbers.Web.Tests.Services
+{
+    [TestClass]
+    public class when_working_with_the_prime_number_service : SpecBase
+    {
+        protected PrimeNumberService _service = new PrimeNumberService();
+    }
+
+    [TestClass]
+    public class and_getting_the_n_first_primes : when_working_with_the_prime_number_service
+    {
+
+
+        [TestMethod]
+        public void then_n_number_of_items_are_returned()
+        {
+
+        }
+    }
+}
