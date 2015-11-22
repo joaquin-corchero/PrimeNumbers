f092707f691ba5f688ffe2e5b60d8a5282a75ca2
Joaquin Corchero <joaquin.corchero@gmail.com>
2015-11-22 00:44:41 +0000

    Test refactoring

--- PrimeNumbers.Web/PrimeNumbers.Web.Tests/Services/PrimeNumberServiceTests.cs
+++ PrimeNumbers.Web/PrimeNumbers.Web.Tests/Services/PrimeNumberServiceTests.cs
@@ -8,35 +8,43 @@ using System.Linq;
 
 namespace PrimeNumbers.Web.Tests.Services
 {
-    [TestClass]
-    public class when_working_with_the_prime_number_service : SpecBase
+    public abstract class when_working_with_the_prime_number_service : SpecBase
     {
         protected PrimeNumberService _service = new PrimeNumberService();
+
+        protected abstract void Execute();
+
     }
 
     [TestClass]
     public class and_getting_the_n_first_primes : when_working_with_the_prime_number_service
     {
+        private List<int> _actual;
+        private int _numberOfPrimes = 20;
+
+        protected override void Execute()
+        {
+            _actual = _service.GetFirstPrimes(_numberOfPrimes);
+        }
+
         [TestMethod]
         public void then_n_number_of_items_are_returned()
         {
             int expected = 5;
+            _numberOfPrimes = expected;
 
-            List<int> actual = _service.GetFirstPrimes(expected);
+            Execute();
 
-            actual.Count().ShouldEqual(expected);
+            _actual.Count().ShouldEqual(expected);
         }
 
         [TestMethod]
         public void then_numbers_are_ordered_from_smaller_to_greater()
         {
-            List<int> actual = _service.GetFirstPrimes(20);
+            Execute();
 
-            for(var i =0; i < actual.Count() -1; i++)
-            {
-                actual[i + 1].ShouldBeGreaterThan(actual[i]);
-            }
+            for (var i = 0; i < _actual.Count() - 1; i++)
+                _actual[i + 1].ShouldBeGreaterThan(_actual[i]);
         }
-
     }
 }
